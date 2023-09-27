f806461feda6b1f2fe7bf26dc4db082a4654a3e6
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-09-27 10:52:41 +0300

    Update exercise 1.py

--- Chapter 1- Getting Started with Python/Exercises/exercise 1.py
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py
@@ -1,6 +1,8 @@
+## Exercise 1: Print Strings :ballot_box_with_check:
 print ("""Twinkle, twinkle, little star,
 	How I wonder what you are! 
 		Up above the world so high,   		
 		Like a diamond in the sky. 
 Twinkle, twinkle, little star, 
 	How I wonder what you are""")
+
