943eb8395c43401639965b249768dcde92d0ab09
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-11 20:58:41 +0300

    Exercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -0,0 +1,5 @@
+f_name = " Sofia "
+m_name = "Gonzales"
+l_name = " Borcelo "
+name = f_name + m_name + l_name
+print (name)
---
+++ practice/Untitled-1.py
@@ -0,0 +1,27 @@
+print ("Name" , "; Sofia " ,34345)
+print ("Mother's Name" , "; Jane Gonzale" , 34536)
+print ("pjm", "xyz", 23915)
+
+#
+name="laurent"
+name="chanel"
+print(name)
+
+#
+print (72/9)
+print (73//9)
+
+#
+age=input("Enter your age:")
+print=(age)
+
+#     
+name1=input("Enter your University Name:")
+print=(name1)
+
+#
+print((2*3/8+(23+5)))
+
+#
+ans=((2*3/8+(23+5)))
+print(int(ans))
