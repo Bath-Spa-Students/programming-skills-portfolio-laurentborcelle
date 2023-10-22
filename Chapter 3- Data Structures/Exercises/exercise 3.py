8f51e9af0b1e9a71c161eabb871b91d0f3deeacf
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:14:31 +0300

    Exercise 3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 3.py
@@ -0,0 +1,11 @@
+# Exercise 3: Your Own List
+
+# Answers:
+
+favorite_cars = ["Aventador", "Huracan", "Gallardo", "Veneno", "Diablo"]
+
+print ("\nMy favorite car model of all time is " + favorite_cars[0] + ".")
+print ("\nOne of the most accelerated horsepowers is the" , favorite_cars[1], "supercar.")
+print ("\nThe", favorite_cars[2], "has had over 14,000 examples over the car's 10 years of production.")
+print ("\nThe rarest production car of this certain brand is " + favorite_cars[3] + ".")
+print ("\nMy godmother owns several " + favorite_cars[4] + ".")
