0978f590bbb4381f4cd7075fcb4345ca7029a30c
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:15:24 +0300

    Exercise 7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 7.py
@@ -0,0 +1,50 @@
+# Exercise 7: Seeing the World 
+
+# Answers:
+
+
+new_list = ["Paris", "Seoul", "New York", "Tokyo", "California"]
+
+print ("Original order:")
+print (new_list)
+
+
+new_list.sort()
+print ("\nAlphabetical:")
+print (new_list)
+
+new_list = ["Paris", "Seoul", "New York", "Tokyo", "California"]
+print ("\nOriginal order:")
+print (new_list)
+
+
+print ("\nReverse Alphabetical:")
+print (sorted(new_list, reverse = True))
+
+
+print ("\nOriginal order:")
+print (new_list)
+
+
+
+new_list.reverse()
+print("\nReversed:")
+print (new_list)
+
+new_list.reverse()
+print("\nReversed:")
+print (new_list)
+
+
+print ("\nOriginal order:")
+new_list.reverse()
+print (new_list)
+
+
+new_list.sort()
+print ("\nAlphabetical:")
+print (new_list)
+
+
+print ("\nReverse Alphabetical:")
+print (sorted(new_list, reverse = True))
