38587dce18c9a09347390e7c5fe5e23b3e320d50
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:10:16 +0300

    Practice 3.py

---
+++ Chapter 1- Getting Started with Python/Practices/practice 3.py
@@ -0,0 +1,5 @@
+# Q3: You need to make a program for a leaderboard. The program needs to output the numbers 1 to 9, each on a separate line  followed by a dot.
+
+# Answers:
+
+print("\n1.\n2.\n3.\n4.\n5.\n6.\n7.\n8.\n9.")
