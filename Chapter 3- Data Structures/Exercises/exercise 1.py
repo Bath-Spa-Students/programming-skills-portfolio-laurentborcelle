c27fdc9493f78e26261c76c7cd252928d58c2a1e
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:14:16 +0300

    Exercise 1.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 1.py
@@ -0,0 +1,12 @@
+# Exercise 1: Names
+
+# Answers:
+
+names = ["Jean", "Paul", "Gaultier","Iris","Van","Harper"]
+
+print (names[0])
+print (names[1])
+print (names[2])
+print (names[3])
+print (names[4])
+print (names[5])
