264a6db2d03f4acef757b52794a63aca04bfefac
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:13:24 +0300

    Exercise 2.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -0,0 +1,5 @@
+# Exercise 2: Variables
+
+# Answers:
+
+print('Cher said, "Until mankind is peaceful enough not to have violence on the news, there’s no point in taking it out of shows that need it for entertainment value."') 
