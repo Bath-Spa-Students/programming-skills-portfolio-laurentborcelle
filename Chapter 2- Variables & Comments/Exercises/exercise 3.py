f62416e8bfe587b17eb77d7bbbf4c038c8a5202a
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:13:37 +0300

    Exercise 3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 3.py
@@ -0,0 +1,17 @@
+# Exercise 3: Stripping Names
+
+# Answers:
+
+name = "\tSofia Borcelo\n"
+
+print ("Unmodified:")
+print (name)
+
+print ("\nUsing lstrip():")
+print (name.lstrip())
+
+print ("\nUsing rstrip():")
+print (name.rstrip())
+
+print ("\nUsing strip():")
+print (name.strip())
