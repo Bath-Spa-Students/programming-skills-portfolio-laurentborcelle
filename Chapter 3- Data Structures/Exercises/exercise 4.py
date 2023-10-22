5cf322943a8b86149e98d0c4bd172188af20701f
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:14:40 +0300

    Exercise 4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 4.py
@@ -0,0 +1,17 @@
+# Exercise 4: Guest List
+
+# Answers:
+
+guest_list = ['Donatella Versace', 'Olivier Rousteing', 'Elsa Schiaparelli', 'Vivienne Westwood']
+
+name = guest_list[0].title()
+print (name + ", we are pleased to invite you to dinner.")
+
+name = guest_list[1].title()
+print (name + ", we are pleased to invite you to dinner.")
+
+name = guest_list[2].title()
+print (name + ", we are pleased to invite you to dinner.")
+
+name = guest_list[3].title()
+print (name + ", we are pleased to invite you to dinner.")
