bd57b7a57673e8341c35b01658a652c7ef13e35b
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:14:23 +0300

    Exercise 2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 2.py
@@ -0,0 +1,23 @@
+# Exercise 2: Greetings
+
+# Answers:
+
+names = ["Jean", "Paul", "Gaultier","Iris","Van","Harper"]
+
+greeting = "Bonjour, " + names[0].title() + "!"
+print (greeting)
+
+greeting = "Bonjour, " + names[1].title() + "!"
+print (greeting)
+
+greeting = "Bonjour, " + names[2].title() + "!"
+print (greeting)
+
+greeting = "Bonjour, " + names[3].title() + "!"
+print (greeting)
+
+greeting = "Bonjour, " + names[4].title() + "!"
+print (greeting)
+
+message = "Bonjour, " + names[5].title() + "!"
+print(message)
