8c48ece684b8db3378e66ea9f4d5ea8b71bcdba6
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:14:03 +0300

    Exercise 5.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 5.py
@@ -0,0 +1,13 @@
+# Exercise 5: USB Shopper
+
+# Answers:
+
+usbstick_cost = 6
+budget = 50
+number_usb_sticks = budget//usbstick_cost
+remaining_budget = budget%usbstick_cost
+
+print ("She can buy", number_usb_sticks, "USB sticks.")
+print ("She will have £", remaining_budget, "left.")
+
+
