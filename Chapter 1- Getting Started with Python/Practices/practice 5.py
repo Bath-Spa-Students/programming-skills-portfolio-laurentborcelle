97f4af97d116178156dc3a49a642a504988f65c6
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-10-22 21:11:25 +0300

    Practice 5.py

---
+++ Chapter 1- Getting Started with Python/Practices/practice 5.py
@@ -0,0 +1,11 @@
+# Q5: Write four strings in different variables and print the output as one string 
+
+# Answers:
+
+string1 = "No"
+string2 = ", "
+string3 = "Gonzales"
+string4 = "!"
+
+all_string = string1 + string2 + string3 + string4
+print(all_string)
