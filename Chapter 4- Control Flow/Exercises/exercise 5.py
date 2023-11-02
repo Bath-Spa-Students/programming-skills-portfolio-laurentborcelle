f5cc885ca8b57a59f6a92c6b5fbaf5a1905a7305
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-02 18:11:48 +0300

    Exercise 5.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 5.py
@@ -0,0 +1,16 @@
+# Exercise 5: Favorite Fruit 
+
+# Answer:
+
+favorite_fruits = ['Strawberry', 'Avocado','Cherry']
+
+if 'Strawberry' in favorite_fruits:
+    print("You really like strawberries!")
+if 'Avocado' in favorite_fruits:
+    print("You really like avocadoes!")
+if 'Peaches' in favorite_fruits:
+    print("You really like peaches!")
+if 'Apples' in favorite_fruits:
+    print("You really like Apples!")
+if 'Cherry' in favorite_fruits:
+    print("You really like cherries!")
