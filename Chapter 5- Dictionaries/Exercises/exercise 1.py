301002b2e826b6e04245582abc5bca9a6a60106c
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-02 18:12:17 +0300

    Exercise 1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise 1.py
@@ -0,0 +1,16 @@
+# Exercise 1: Person
+
+# Answer:
+
+dictionary = {
+    'first_name': 'Cindy',
+    'last_name': 'Kimberly',
+    'age': 24,
+    'city': 'Costa Blanca'
+}
+
+print ("First Name: " + dictionary['first_name'])
+print ("Last Name: " + dictionary['last_name'])
+print ("Age: " + str(dictionary['age']))  # Convert age to a string for printing
+print ( "City: " + dictionary['city'])
+
