29825e2fa27659efe65d25886c86c987beabe962
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-02 18:10:48 +0300

    Exercise 1.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 1.py
@@ -0,0 +1,13 @@
+# Exercise 1:  Alien Colors #1
+
+# Answer:
+
+# Passing the if test
+alien_color = "green"
+if alien_color == "green":
+    print ("You just earned 5 points!")
+
+# Failing the if test
+alien_color = "red"
+if alien_color == "green":
+    print ("You just earned 5 points!")
