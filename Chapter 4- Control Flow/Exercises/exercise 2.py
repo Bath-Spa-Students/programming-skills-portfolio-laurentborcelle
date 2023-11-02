95166ad21aad64560cb3239fbcbd4a89999fe611
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-02 18:11:01 +0300

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise 2.py
@@ -0,0 +1,11 @@
+# Exercise 2: Alien Colors #2 
+
+# Answer:
+
+
+alien_color = input("Enter the color of the alien shot: ")
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+else:
+    print("You just earned 10 points!")
