1ff86b8a4b6e7e01ebe8fb55da2ad2eb9b9abb86
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-15 19:08:56 +0300

    Exercise 3.py

---
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -0,0 +1,10 @@
+# Exercise 3: T-Shirt
+
+# Answers:
+
+def make_shirt(size, message):
+    print (f"\nI'm going to make a {size} shirt.")
+    print (f'The shirt will be printed with, "{message}"')
+
+make_shirt (message = "Tailor of Chaos.", size = 'medium')
+make_shirt ('small', 'Born Stunner.')
