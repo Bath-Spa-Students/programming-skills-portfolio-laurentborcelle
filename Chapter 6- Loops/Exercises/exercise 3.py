b2e7f3937a1cc501ac7d021354499aab51b314cd
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-15 19:07:37 +0300

    Exercise 3.py

---
+++ Chapter 6- Loops/Exercises/exercise 3.py
@@ -0,0 +1,18 @@
+# Exercise 3: Infinity
+
+# Answers:
+
+while True:
+
+    age = input("Enter your age: ")
+    
+    age = int(age)
+    if age < 3:
+        print ("Your ticket is free.")
+    elif age < 13:
+        print ("The ticket costs $10.")
+    else:
+        print ("The ticket costs $15.")
+    
+
+    
