9a0abc6a8d40968e67a132876e4766f7c51424f1
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-15 19:09:04 +0300

    Exercise 4.py

---
+++ Chapter 7- Functions/Exercises/exercise 4.py
@@ -0,0 +1,11 @@
+# Exercise 4:  Large Shirts
+
+# Answers:
+
+def make_shirt(size = 'large', message = 'I love Python.'):
+    print (f"\nI am going to make a {size} shirt.")
+    print (f'The shirt will be printed with, "{message}"')
+
+make_shirt()
+make_shirt ( size = 'medium')
+make_shirt ('small', 'Python is mediocre.')
