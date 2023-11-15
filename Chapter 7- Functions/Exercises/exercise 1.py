bb2ec68293357c538bc60bc1bad29c37c1fdc113
laurentborcelle <144213411+laurentborcelle@users.noreply.github.com>
2023-11-15 19:08:07 +0300

    Exercise 1.py

---
+++ Chapter 7- Functions/Exercises/exercise 1.py
@@ -0,0 +1,9 @@
+# Exercise 1:   Message 
+
+# Answers:
+
+def display_message():
+    message = "Within this chapter, I am learning about using Functions through codes used to perform a specific task."
+    print (message)
+
+display_message()
